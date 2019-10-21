cfa67836012302a9bd08a976364ee15897a8dfdd
ZbigniewPowierza <zbigniew.powierza@gpw.pl>
2019-10-21 19:50:55 +0200

    zmiany w skryptach

---
+++ napisy.py
@@ -0,0 +1,11 @@
+#
+# Typ danych: napisy (albo string)
+#
+napis1 = "Ala ma kota"
+napis2 = 'Ala ma kota'
+napis3 = 'A to jest "cudzysłów" '
+napis4 = "A to jest \"cudzysłów\" "
+napus5 = "znaki specjalne: \t \n \r "
+
+dlugosc = len(napis1)
+print("Długość zmiennej napis1 to ", dlugosc, " znaków")
