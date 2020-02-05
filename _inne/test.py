b233074c8872be77bd0cea81a364cd3187bf481d
ZbigniewPowierza <zbigniew.powierza@gpw.pl>
2020-02-05 19:23:01 +0100

    testy

---
+++ _inne/test.py
@@ -0,0 +1,14 @@
+statuses = {
+    "Alice": "online",
+    "Bob": "offline",
+    "Eve": "online",
+}
+
+
+def online_count(slownik, status):
+    ile = 0
+    for i in slownik:
+        if slownik[i] == "status":
+            ile = ile + 1
+        return ile
+print(online_count(statuses, "online"))
