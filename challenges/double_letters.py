2c2bae3e6c33b67ae0e20d4126cc61d625e1303f
Zbigniew Powierża <zbigniew.powierza@gpw.pl>
2020-02-10 01:07:10 +0100

    2020-02-09

--- challenges/double_letters.py
+++ challenges/double_letters.py
@@ -1,6 +1,7 @@
 a = "aabcdee"
-
 for i, j in enumerate(a[:-1]):
     print(f"Numer {i}  - znak '{j}'")
     if j == a[i + 1]:
-        print(f"Znak nr {i} w {len(a)}-znakowym stringu to '{a}' to litera '{j}' i jest równy znakowi następnemu, czyli znakowi numer {i + 1}")
+        print(f"Znak nr {i} w {len(a)}-znakowym stringu '{a}' to znak '{j}' i jest równy znakowi następnemu, czyli znakowi numer {i + 1}")
+    else:
+        print(f"Znak nr {i} w {len(a)}-znakowym stringu '{a}' to znak '{j}' i NIE JEST on równy znakowi następnemu, czyli znakowi numer {i + 1}")
